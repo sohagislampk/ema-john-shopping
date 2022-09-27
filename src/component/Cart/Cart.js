4b364ef9df247bc895640e538e7926acb1f6f2b3
sohagislampk <sohagislampk@gmail.com>
2022-09-27 16:46:34 +0600

    cart calculation and local storage done

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -5,16 +5,18 @@ const Cart = (props) => {
 
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price
-        shipping = shipping + product.shipping
+        quantity = product.quantity + quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
     }
     const tax = parseFloat((total * 0.1).toFixed(2));
     const grandTotal = total + shipping + tax
     return (
         <div className='cart'>
             <h2>Order Summery</h2>
-            <p>Seleced Items : {cart.length}</p>
+            <p>Seleced Items : {quantity}</p>
             <p>Total Price : ${total}</p>
             <p>Total Shipping : ${shipping}</p>
             <p>Total Tax : ${tax} </p>
--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -25,10 +25,21 @@ const Shop = () => {
         setCart(savedCart);
     }, [products])
 
-    function addToCart(product) {
-        const newCart = [...cart, product];
+    function addToCart(selectedProduct) {
+        let newCart = [];
+        const exist = cart.find(product => product.id === selectedProduct.id)
+        if (!exist) {
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else {
+            const rest = cart.filter(product => product.id !== selectedProduct.id)
+            exist.quantity = exist.quantity + 1;
+            newCart = [...rest, exist];
+
+        }
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(selectedProduct.id);
     }
     return (
         <div className='shop-container'>
